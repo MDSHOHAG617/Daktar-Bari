66859835ac20f3bfa616d58514f7bd3ab9673ff1
Md Shohag <mdshohag119764@gmail.com>
2022-12-06 20:46:03 +0600

    protections plans updated

--- src/Components/Home/CareAndProtectionPlant.js
+++ src/Components/Home/CareAndProtectionPlant.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { TiTickOutline } from "react-icons/ti";
+import { ImFire } from "react-icons/im";
 const CareAndProtectionPlant = () => {
   return (
     <div>
-      <div className="plans grid gap-4 grid-cols-1 lg:grid-cols-3 mx-20 mb-10 ">
-        <div className="plan bg-white p-4 rounded">
+      <div className="plans grid gap-6 grid-cols-1 lg:grid-cols-3 mx-20 mb-10 ">
+        <div className="plan bg-white p-4 rounded-xl relative ">
           <div class="flex justify-between  items-start  ">
             <div>
               <h1 className="text-left text-xl text-primary">
                 Daktar bari Care
               </h1>
-              <p className="text-left">
+              <p className="text-left ">
                 <span className="text-xl font-mono">৳ 114 </span>
                 <span className="text-sm font-semibold">/month</span>
               </p>
@@ -23,7 +24,7 @@ const CareAndProtectionPlant = () => {
               ></img>
             </div>
           </div>
-          <div className=" text-left ">
+          <div className=" text-left mb-16">
             <p className="flex items-start font-normal mb-3">
               <span className="">
                 <TiTickOutline />
@@ -38,18 +39,25 @@ const CareAndProtectionPlant = () => {
               Upto 40% discount on home diagnostic tests.
             </p>
           </div>
-          <button className="btn btn-primary text-white btn-sm float-left mt-12">
+          <button className="btn btn-primary text-white btn-sm float-left  absolute bottom-4 left-4">
             Subscribe now
           </button>
         </div>
-        <div className="plan bg-white p-4 rounded">
+        <div className="plan bg-white p-4 rounded-xl relative">
           <div class="flex justify-between  items-start  ">
             <div>
+              <div className="relative ">
+                <h1 className="most-popular absolute bottom-1 bg-[#FF0000] text-white px-3 py-0.5 rounded-md font-semibold text-sm flex items-center ">
+                  <ImFire className="text-white text-xs mr-1" />
+                  Most Popular
+                </h1>
+              </div>
+
               <h1 className="text-left text-xl text-primary">
-                Daktar bari Care
+                Daktar bari Care Plus
               </h1>
               <p className="text-left">
-                <span className="text-xl font-mono">৳ 114 </span>
+                <span className="text-xl font-mono">৳ 200</span>
                 <span className="text-sm font-semibold">/month</span>
               </p>
               <p className="text-left font-normal mt-2 mb-6">For 1 adult </p>
@@ -61,7 +69,7 @@ const CareAndProtectionPlant = () => {
               ></img>
             </div>
           </div>
-          <div className=" text-left ">
+          <div className=" text-left mb-16">
             <p className="flex items-start font-normal mb-3">
               <span className="">
                 <TiTickOutline />
@@ -75,22 +83,30 @@ const CareAndProtectionPlant = () => {
               </span>
               Upto 40% discount on home diagnostic tests.
             </p>
+            <p className="flex items-start font-normal">
+              <span className="">
+                <TiTickOutline />
+              </span>
+              ৳ 40,000 cashback ons health & life insurance.
+            </p>
           </div>
-          <button className="btn btn-primary text-white btn-sm float-left mt-12">
+          <button className="btn btn-primary text-white btn-sm float-left  absolute bottom-4 left-4">
             Subscribe now
           </button>
         </div>
-        <div className="plan bg-white p-4 rounded">
+        <div className="plan bg-white p-4 rounded-xl relative">
           <div class="flex justify-between  items-start  ">
             <div>
               <h1 className="text-left text-xl text-primary">
-                Daktar bari Care
+                Daktar bari Family Care
               </h1>
               <p className="text-left">
-                <span className="text-xl font-mono">৳ 114 </span>
+                <span className="text-xl font-mono">৳ 309 </span>
                 <span className="text-sm font-semibold">/month</span>
               </p>
-              <p className="text-left font-normal mt-2 mb-6">For 1 adult </p>
+              <p className="text-left font-normal mt-2 mb-6">
+                For 3 family members{" "}
+              </p>
             </div>
             <div>
               <img
@@ -99,7 +115,7 @@ const CareAndProtectionPlant = () => {
               ></img>
             </div>
           </div>
-          <div className=" text-left ">
+          <div className=" text-left mb-16">
             <p className="flex items-start font-normal mb-3">
               <span className="">
                 <TiTickOutline />
@@ -114,18 +130,18 @@ const CareAndProtectionPlant = () => {
               Upto 40% discount on home diagnostic tests.
             </p>
           </div>
-          <button className="btn btn-primary text-white btn-sm float-left mt-12">
+          <button className="btn btn-primary text-white btn-sm float-left  absolute bottom-4 left-4 ">
             Subscribe now
           </button>
         </div>
-        <div className="plan bg-white p-4 rounded">
+        <div className="plan bg-white p-4 rounded-xl relative">
           <div class="flex justify-between  items-start  ">
             <div>
               <h1 className="text-left text-xl text-primary">
-                Daktar bari Care
+                Daktar bari Special Care
               </h1>
               <p className="text-left">
-                <span className="text-xl font-mono">৳ 114 </span>
+                <span className="text-xl font-mono">৳ 451 </span>
                 <span className="text-sm font-semibold">/month</span>
               </p>
               <p className="text-left font-normal mt-2 mb-6">For 1 adult </p>
@@ -137,7 +153,7 @@ const CareAndProtectionPlant = () => {
               ></img>
             </div>
           </div>
-          <div className=" text-left ">
+          <div className=" text-left mb-16">
             <p className="flex items-start font-normal mb-3">
               <span className="">
                 <TiTickOutline />
@@ -149,21 +165,21 @@ const CareAndProtectionPlant = () => {
               <span className="">
                 <TiTickOutline />
               </span>
-              Upto 40% discount on home diagnostic tests.
+              Free 4 consultations with specialists in a year
             </p>
           </div>
-          <button className="btn btn-primary text-white btn-sm float-left mt-12">
+          <button className="btn btn-primary text-white btn-sm float-left  absolute bottom-4 left-4 ">
             Subscribe now
           </button>
         </div>
-        <div className="plan bg-white p-4 rounded">
+        <div className="plan bg-white p-4 rounded-xl relative">
           <div class="flex justify-between  items-start  ">
             <div>
               <h1 className="text-left text-xl text-primary">
-                Daktar bari Care
+                Daktar bari Premium Care
               </h1>
               <p className="text-left">
-                <span className="text-xl font-mono">৳ 114 </span>
+                <span className="text-xl font-mono">৳ 570 </span>
                 <span className="text-sm font-semibold">/month</span>
               </p>
               <p className="text-left font-normal mt-2 mb-6">For 1 adult </p>
@@ -175,7 +191,7 @@ const CareAndProtectionPlant = () => {
               ></img>
             </div>
           </div>
-          <div className=" text-left ">
+          <div className=" text-left mb-16">
             <p className="flex items-start font-normal mb-3">
               <span className="">
                 <TiTickOutline />
@@ -187,10 +203,16 @@ const CareAndProtectionPlant = () => {
               <span className="">
                 <TiTickOutline />
               </span>
-              Upto 40% discount on home diagnostic tests.
+              Free 4 consultations with specialists in a year
+            </p>
+            <p className="flex items-start font-normal">
+              <span className="">
+                <TiTickOutline />
+              </span>
+              ৳ 350,000 cashback on health & life insurance.
             </p>
           </div>
-          <button className="btn btn-primary text-white btn-sm float-left mt-12">
+          <button className="btn btn-primary text-white btn-sm float-left  absolute bottom-4 left-4 ">
             Subscribe now
           </button>
         </div>
