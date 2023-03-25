7b8c0d688b1f1288daecc65769ba1eb1d7ba74e9
Md Shohag <mdshohag119764@gmail.com>
2023-03-25 16:23:25 +0600

    health plans updated from json file

--- src/Components/HealthPlans/HealthPlan.js
+++ src/Components/HealthPlans/HealthPlan.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { TiTick } from "react-icons/ti";
 import care from "../../images/care.png";
 
 const HealthPlan = ({ plan }) => {
-  console.log(plan);
+  const { services } = plan;
+  // console.log(services);
   return (
     <div>
-      <div className="plan card-body hover: hover:opacity-80 p-4 rounded-xl relative card shadow-2xl">
+      <div className="plan card-body hover: hover:opacity-80 p-4 rounded-xl relative card shadow-2xl h-full">
         <div class="flex justify-between  items-start  ">
           <div>
             <h1 className="text-left text-xl text-primary">{plan.name}</h1>
@@ -21,19 +22,16 @@ const HealthPlan = ({ plan }) => {
             <img className="w-14 " src={plan.img}></img>
           </div>
         </div>
+        {/* services */}
         <div className=" text-left mb-16">
-          <p className="flex items-start font-normal mb-3">
-            <span className="">
-              <TiTick className=" mt-1 text-lg" />
-            </span>
-            {plan.services[0].service}
-          </p>
-          <p className="flex items-start font-normal">
-            <span className="">
-              <TiTick className=" mt-1 text-lg" />
-            </span>
-            {plan.services[1].service}
-          </p>
+          {services.map((service) => (
+            <p className="flex items-start font-normal mb-3">
+              <span className="">
+                <TiTick className=" mt-1 text-lg" />
+              </span>
+              {service.service}
+            </p>
+          ))}
         </div>
         <button className="btn btn-primary text-white btn-sm float-left  absolute bottom-4 left-4">
           {plan.button}
