783e5b655d999583a8afdf8aef3eb894cc7aacc9
Md Shohag <mdshohag119764@gmail.com>
2023-04-03 18:31:53 +0600

    doctor reg updated

--- src/Components/Home/ForDoctors.js
+++ src/Components/Home/ForDoctors.js
@@ -1,194 +1,25 @@
 import React from "react";
-import {
-  useCreateUserWithEmailAndPassword,
-  useSignInWithGoogle,
-  useUpdateProfile,
-} from "react-firebase-hooks/auth";
-import auth from "../../firebase.init";
-import { useForm } from "react-hook-form";
-// import Loading from "../Shared/Loading";
-import { Link, useNavigate } from "react-router-dom";
-// import useToken from "../../hooks/useToken";
-// import login from "../../../images/login.svg";
-// import signUp from "../../../images/secureLogin.svg";
-import mobilelogin from "../../images/mobilelogin.svg";
 
 const ForDoctors = () => {
-  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-  const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
-
-  const [updateProfile, updating, updateError] = useUpdateProfile(auth);
-
-  // const [token] = useToken(user || gUser);
-
-  const navigate = useNavigate();
-
-  let signInError;
-
-  if (loading || gLoading || updating) {
-    // return <Loading></Loading>;
-  }
-
-  if (error || gError || updateError) {
-    signInError = (
-      <p className="text-red-500">
-        <small>
-          {error?.message || gError?.message || updateError?.message}
-        </small>
-      </p>
-    );
-  }
-
-  // if (token) {
-  //   navigate("/");
-  // }
-
-  if (user || gUser) {
-    navigate("/");
-  }
-
-  const onSubmit = async (data) => {
-    await createUserWithEmailAndPassword(data.email, data.password);
-    await updateProfile({ displayName: data.name });
-    // console.log("update done");
-  };
   return (
-    <div>
+    <div className="mt-32 px-16">
+      <h1 className="font-bold text-2xl lg:text-5xl lg:text-left">
+        Are You <br></br> A Qualified Doctor?
+      </h1>
+      <h2 className="font-bold text-lg  lg:text-2xl lg:text-left mt-8 mb-8">
+        Join the forefront of digital healthcare
+      </h2>
       <div className="">
-        <div className=" flex  justify-around items-center h-screen my-4 ">
-          <div className="">
-            <img
-              src="https://drsafehands.com/resources/assets/images/doctor.png"
-              className=""
-            />
-          </div>
-
-          <div className="card w-96 ">
-            <div className="card-body">
-              <h2 className="text-center text-2xl font-bold">
-                Doctor Registration
-              </h2>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-control w-full max-w-xs">
-                  <label className="label">
-                    <span className="label-text">Name</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Your Name"
-                    className="input input-bordered w-full max-w-xs"
-                    {...register("name", {
-                      required: {
-                        value: true,
-                        message: "Name is Required",
-                      },
-                    })}
-                  />
-                  <label className="label">
-                    {errors.name?.type === "required" && (
-                      <span className="label-text-alt text-red-500">
-                        {errors.name.message}
-                      </span>
-                    )}
-                  </label>
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="email"
-                    placeholder="Your Email"
-                    className="input input-bordered w-full max-w-xs"
-                    {...register("email", {
-                      required: {
-                        value: true,
-                        message: "Email is Required",
-                      },
-                      pattern: {
-                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                        message: "Provide a valid Email",
-                      },
-                    })}
-                  />
-                  <label className="label">
-                    {errors.email?.type === "required" && (
-                      <span className="label-text-alt text-red-500">
-                        {errors.email.message}
-                      </span>
-                    )}
-                    {errors.email?.type === "pattern" && (
-                      <span className="label-text-alt text-red-500">
-                        {errors.email.message}
-                      </span>
-                    )}
-                  </label>
-                </div>
-                <div className="form-control w-full max-w-xs">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    className="input input-bordered w-full max-w-xs"
-                    {...register("password", {
-                      required: {
-                        value: true,
-                        message: "Password is Required",
-                      },
-                      minLength: {
-                        value: 6,
-                        message: "Must be 6 characters or longer",
-                      },
-                    })}
-                  />
-                  <label className="label">
-                    {errors.password?.type === "required" && (
-                      <span className="label-text-alt text-red-500">
-                        {errors.password.message}
-                      </span>
-                    )}
-                    {errors.password?.type === "minLength" && (
-                      <span className="label-text-alt text-red-500">
-                        {errors.password.message}
-                      </span>
-                    )}
-                  </label>
-                </div>
-
-                {signInError}
-                <input
-                  className="btn w-full max-w-xs text-white"
-                  type="submit"
-                  value="Sign Up"
-                />
-              </form>
-              <p>
-                <small>
-                  Already have an account?{" "}
-                  <Link className="text-blue-500" to="/login">
-                    Please login
-                  </Link>
-                </small>
-              </p>
-              <div className="divider">OR</div>
-              <button
-                onClick={() => signInWithGoogle()}
-                className="btn btn-outline"
-              >
-                Continue with Google
-              </button>
-            </div>
-          </div>
-        </div>
+        <p className="font-normal text-lg font-semibold lg:text-left ">
+          Join DocTime network and create your virtual chamber provide medical
+          consultancy via video call and expand the reach of your service.{" "}
+        </p>
+        <p className="mt-8 font-normal text-lg font-semibold lg:text-left">
+          Enroll yourself just in minutes!
+        </p>
+        <button className="btn rounded-full bg-[#07C0BA] border-none  text-white px-8 btn-md float-left	">
+          Sign up Now
+        </button>
       </div>
     </div>
   );
